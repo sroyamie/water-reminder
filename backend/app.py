dab216807e78f166b0bee06635225d287cc6700d
Amie <sroyamie@gmail.com>
2026-07-29 18:44:39 +0530

    Prepare backend for deployment

--- backend/app.py
+++ backend/app.py
@@ -71,4 +71,6 @@ def get_history():
     return jsonify(history)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", debug=False, port=port)
